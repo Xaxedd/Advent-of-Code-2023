c769366856bed8f0929322f8e50d067722058695
Xaxedd <sebastiankostr@gmail.com>
2023-12-03 15:56:55 +0100

    working version with no prints

--- day3/day3_normal.py
+++ day3/day3_normal.py
@@ -1,5 +1,6 @@
 import time
 
+
 def main():
     puzzle_input = open("D:\python projects\AdventOfCode2023\day3\puzzle_input.txt", "r", encoding="utf8").readlines()
     new_puzzle = []
@@ -12,37 +13,24 @@ def main():
     for line_nr, line in enumerate(puzzle_input):
         line = line.strip()
         for char_nr, char in enumerate(line):
-            a = 1
             if not char.isdigit() and char != ".":
                 numbers_near_this_symbol = []
-                for y in range(line_nr-2, line_nr+3):
-                    xxx = ""
-                    for x in range(char_nr-5, char_nr+7):
-                        if x > len(line)-1 or y > len(new_puzzle) - 1:
-                            continue
-                        xxx += str(new_puzzle[y][x])
-                    print(xxx)
-
-
-                print(f"symbol cords: y{line_nr} x{char_nr}")
-                for y in range(line_nr-1, line_nr+2):
+                for y in range(line_nr - 1, line_nr + 2):
                     a = 1
-                    for x in range(char_nr-1, char_nr+2):
-                        print(f"cord checking y{y} x{x}")
+                    for x in range(char_nr - 1, char_nr + 2):
                         if a > 1:
                             a -= 1
                             continue
-                        print(puzzle_input[y][x])
                         if puzzle_input[y][x].isdigit():
                             number = str(puzzle_input[y][x])
                             new_puzzle[y][x] = "."
                             while True:
-                                if puzzle_input[y][x-a].isdigit():
-                                    number = str(puzzle_input[y][x-a]) + number
+                                if puzzle_input[y][x - a].isdigit():
+                                    number = str(puzzle_input[y][x - a]) + number
                                     new_puzzle[y][x - a] = "."
                                 else:
                                     break
-                                a+=1
+                                a += 1
 
                             a = 1
                             while True:
@@ -52,28 +40,12 @@ def main():
                                 else:
                                     break
                                 a += 1
-                            print(number)
                             suma += int(number)
                             numbers_near_this_symbol.append(int(number))
                 if char == "*":
                     if len(numbers_near_this_symbol) == 2:
                         gear_ratio += (numbers_near_this_symbol[0] * numbers_near_this_symbol[1])
 
-                for y in range(line_nr-2, line_nr+3):
-                    xxx = ""
-                    for x in range(char_nr-5, char_nr+7):
-                        if x > len(line)-1 or y > len(new_puzzle) - 1:
-                            continue
-                        xxx += str(new_puzzle[y][x])
-
-                    print(xxx)
-                print("\n"*2)
-
-    for y in new_puzzle:
-        xxx = ""
-        for x in y:
-            xxx += x
-        print(xxx)
     print("part one answer:", suma)
     print("part two answer:", gear_ratio)
 
